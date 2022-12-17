67d634a2ff058a0790da03bfa307908da87e907c
Alexander Tepe <a.tepe@kalverkamp.de>
2022-12-17 20:14:12 +0100

    wip frontend deployment

---
+++ registry-web/src/main/java/de/applegreen/registry/web/controller/NavigationController.java
@@ -0,0 +1,16 @@
+package de.applegreen.registry.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Alexander Tepe | a.tepe@kalverkamp.de
+ */
+@Controller
+public class NavigationController {
+
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+}

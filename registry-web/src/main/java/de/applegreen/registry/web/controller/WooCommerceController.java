5094bbb751e7f3b660c7be364f37140e7549f106
Alexander Tepe <a.tepe@kalverkamp.de>
2023-02-02 16:10:59 +0100

    added some controller stubs, details endpoint for purchase

---
+++ registry-web/src/main/java/de/applegreen/registry/web/controller/WooCommerceController.java
@@ -0,0 +1,15 @@
+package de.applegreen.registry.web.controller;
+
+import de.applegreen.registry.business.util.HasLogger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Alexander Tepe | a.tepe@hotmail.de
+ */
+@Controller
+@RequestMapping("/woocommerce")
+public class WooCommerceController implements HasLogger {
+
+    // TODO
+}

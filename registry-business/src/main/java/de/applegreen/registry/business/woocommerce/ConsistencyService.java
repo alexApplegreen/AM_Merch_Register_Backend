1402e8d5f341157b779d8285e63ac19d8c46eecf
Alexander Tepe <a.tepe@kalverkamp.de>
2023-03-22 22:28:04 +0100

    Implemented Advice Annotation

--- registry-business/src/main/java/de/applegreen/registry/business/woocommerce/ConsistencyService.java
+++ registry-business/src/main/java/de/applegreen/registry/business/woocommerce/ConsistencyService.java
@@ -1,29 +1,89 @@
 package de.applegreen.registry.business.woocommerce;
 
 import de.applegreen.registry.business.util.HasLogger;
+import de.applegreen.registry.model.ProductEntity;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
- * @author Alexander Tepe | a.tepe@kalverkamp.de
+ * @author Alexander Tepe | a.tepe@hotmail.de
  */
 @Aspect
 @Service
 public class ConsistencyService implements HasLogger {
 
     @Value("${woocommerce_api_key}")
-    private String key;
+    private String KEY;
 
-    private final String baseUrl = "https://aboutmonsters.de/wp-json/wc/v3";
+    @Value("${woocommerce_api_secret}")
+    private String SECRET;
+
+    private final static String BASE_URL = "https://aboutmonsters.de/wp-json/wc/v3";
+    private final static String PRODUCTS = "/products";
+    private final static String STOCK_KEY = "stock_quantity";
 
     /**
      * Pointcut definition and advice for when a purchase has been registered
      */
+    @SuppressWarnings("rawtypes")
     @AfterReturning(value = "@annotation(de.applegreen.registry.business.util.AdviceAnnotations.PurchaseCommit)")
-    public void updateShopRegistry() {
-        // TODO actually send API Request to woocommerce instance
-        this.getLogger().info("Updating Registry of webshop");
+    public void updateShopRegistry(JoinPoint joinPoint) {
+        // TODO passt noch nicht
+        Optional<ProductEntity> productEntityOptional = Arrays.stream(
+                (ProductEntity[]) joinPoint.getArgs()
+        ).findFirst();
+        if (productEntityOptional.isEmpty()) {
+            this.getLogger().warn("No Argument present in Advice");
+            return;
+        }
+        ProductEntity productEntity = productEntityOptional.get();
+
+        RestTemplate restTemplate = new RestTemplate();
+        String plainauth = this.KEY + ":" + this.SECRET;
+        byte[] plainAuthBytes = plainauth.getBytes();
+        byte[] base64auth = Base64.getEncoder().encode(plainAuthBytes);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + new String(base64auth));
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        Long productId = productEntity.getId();
+        this.getLogger().info("Updating Stock of Product " + productId);
+        ResponseEntity<Map> responsedata = restTemplate.exchange(
+                ConsistencyService.BASE_URL + ConsistencyService.PRODUCTS + productId,
+                HttpMethod.GET,
+                httpEntity,
+                Map.class
+        );
+        if (!responsedata.getStatusCode().is2xxSuccessful()) {
+            this.getLogger().warn("Cannot get Data from WooCommerce Server");
+            // TODO save in database and retry exponentially
+            return;
+        }
+        Long quantity = (Long) responsedata.getBody().get(ConsistencyService.STOCK_KEY);
+        quantity = quantity - 1;
+        Map<String, Long> data = new HashMap<>();
+        data.put(ConsistencyService.STOCK_KEY, quantity);
+        restTemplate.exchange(
+                ConsistencyService.BASE_URL + ConsistencyService.PRODUCTS + productId,
+                HttpMethod.PUT,
+                httpEntity,
+                Map.class,
+                data
+        );
+        this.getLogger().info("Updated stock quantity in database");
     }
 }
